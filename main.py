788ef4da192554691db4cbd09b1e4ef95c4ca467
Jacky Lee <dr.jackylee@gmail.com>
2017-03-04 22:50:07 -0800

    Enable processing of individual youtube videos

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from training_data import messages
-from analyze_sentiment import get_classifier, analyze
 from flask import Flask, render_template, request
+from training_data import training_messages
+from analyze_sentiment import get_classifier, analyze
+from comment_scrape import get_video_comments
 app = Flask(__name__)
 
 @app.route('/')
 def main():
     return render_template('individual.html')
 
-@app.route('/individual')
+@app.route('/individual', methods=['POST'])
 def individual():
-    return render_template('individual.html')
+    classifier = get_classifier(training_messages)
+    url = request.form['video-url']
+    video_comments = get_video_comments(url)
+    comments_score = 0
+    for comment in video_comments:
+        comments_score += analyze(message, classifier)
+    return render_template('individual.html', comments_score = comments_score)
 
 @app.route('/comparison')
 def comparison():
@@ -17,7 +24,7 @@ def comparison():
 
 @app.route('/sentiment', methods=['POST'])
 def sentiment():
-    classifier = get_classifier(messages)
+    classifier = get_classifier(training_messages)
     message = request.form['message']
     test_sentiment = analyze(message, classifier)
     return render_template('index.html', test_sentiment=test_sentiment)
