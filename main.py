02376a2fc21e3bff5aee8bb15f0d85755c3bd937
Jacky Lee <dr.jackylee@gmail.com>
2017-03-05 04:56:22 -0800

    Use ajax to update page

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from training_data import training_messages
 from analyze_sentiment import get_classifier, analyze
-from comment_scrape import get_video_comments
+from comment_scrape import get_video_comments, get_embed_id
 app = Flask(__name__)
 
 @app.route('/')
@@ -23,16 +23,23 @@ def individual():
                            normalized_score = normalized_score,
                            url = url)
 
-@app.route('/comparison')
-def comparison():
-    return render_template('comparison.html')
+#@app.route('/comparison')
+#def comparison():
+#    return render_template('comparison.html')
 
-@app.route('/sentiment', methods=['POST'])
-def sentiment():
+@app.route('/ajax', methods = ['POST'])
+def ajax_request():
     classifier = get_classifier(training_messages)
-    message = request.form['message']
-    test_sentiment = analyze(message, classifier)
-    return render_template('index.html', test_sentiment=test_sentiment)
+    full_url = request.form['url']
+    url = get_embed_id(full_url)
+
+    video_comments = get_video_comments(full_url)
+    comments_score = 0
+    for comment in video_comments:
+        comments_score += analyze(comment, classifier)
+    normalized_score = comments_score / len(video_comments)
+
+    return jsonify(url = url, normalized_score = normalized_score)
 
 if __name__ == '__main__':
     app.run(debug=True)
