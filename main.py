4be159ac91aeaf7cfe11f0526a2d62957417e5c6
Jacky Lee <dr.jackylee@gmail.com>
2017-03-05 08:49:36 -0800

    Add ajax to comparison tab

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def comparison():
 def rankings():
     return render_template('rankings.html')
 
-@app.route('/ajax', methods = ['POST'])
-def ajax_request():
+@app.route('/individual_ajax', methods = ['POST'])
+def individual_ajax_request():
     classifier = get_classifier(training_messages)
     full_url = request.form['url']
     url = get_embed_id(full_url)
@@ -34,6 +34,30 @@ def ajax_request():
     normalized_score = comments_score / len(video_comments)
 
     return jsonify(url = url, normalized_score = normalized_score)
+
+@app.route('/comparison_ajax', methods = ['POST'])
+def comparison_ajax_request():
+    classifier = get_classifier(training_messages)
+
+    full_url_1 = request.form['url-1']
+    url1 = get_embed_id(full_url_1)
+
+    video_comments_1 = get_video_comments(full_url_1)
+    comments_score_1 = 0
+    for comment in video_comments_1:
+        comments_score_1 += analyze(comment, classifier)
+    normalized_score_1 = comments_score_1 / len(video_comments_1)
+
+    full_url_2 = request.form['url-2']
+    url2 = get_embed_id(full_url_2)
+
+    video_comments_2 = get_video_comments(full_url_2)
+    comments_score_2 = 0
+    for comment in video_comments_2:
+        comments_score_2 += analyze(comment, classifier)
+    normalized_score_2 = comments_score_2 / len(video_comments_2)
+
+    return jsonify(url1 = url1, url2 = url2, normalized_score_1 = normalized_score_1, normalized_score_2 = normalized_score_2)
 
 if __name__ == '__main__':
     app.run(debug=True)
