05b214d80946dda6d0360fd9b75003501df0111b
Jacky Lee <dr.jackylee@gmail.com>
2017-03-20 09:43:17 -0700

    Modify training data
    Use only first 500 lines to speed up data analysis

--- training_data.py
+++ training_data.py
@@ -1,3 +1,4 @@
 from parse_csv import parse_csv
 
 training_messages = parse_csv("https://storage.googleapis.com/sentiment-analysis-dataset/training_data.csv")
+training_messages = training_messages[:500]
